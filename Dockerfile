1519b6ab9c028df55051a4a8cc06e7dad2e0e8f7
sehe <sgheeren@gmail.com>
2022-07-20 23:59:18 +0200

    q73024824 not using conan

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,13 @@ ENV DEBIAN_FRONTEND=noninteractive
 RUN apt -qyyy update
 RUN apt -qyyy install build-essential cmake
 RUN apt -qyyy install python3-pip
-RUN pip3 install conan
+RUN apt -qyyy install libboost-all-dev libsnappy-dev git
 COPY work work
 WORKDIR work
+RUN git clone https://github.com/apache/avro
 RUN mkdir build
 WORKDIR build
-RUN conan install ..
 RUN cmake .. -DCMAKE_BUILD_TYPE=Debug
-RUN cmake --build .
+RUN cmake --build . -j
 
-CMD ./bin/sotest
+CMD ./sotest
--- work/test.cpp
+++ work/test.cpp
@@ -1,15 +1,79 @@
-#include <boost/filesystem.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <fstream>
-#include <iostream>
 #include <string>
+//#include "bla.hh"
+#include "api/Compiler.hh"
+#include "api/DataFile.hh"
+#include "api/Decoder.hh"
+#include "api/Encoder.hh"
+#include "api/Generic.hh"
+#include "api/Stream.hh"
 
-using namespace std;
-using namespace boost::filesystem;
+namespace c {
+    struct cpx {
+        std::string re;
+        std::string im;
+        cpx() :
+            re(std::string()),
+            im(std::string())
+        { }
+    };
+}
+
+namespace avro {
+template<>
+struct codec_traits<c::cpx>
+{
+    static void encode(Encoder& e, const c::cpx& v)
+    {
+        avro::encode(e, v.re);
+        avro::encode(e, v.im);
+    }
+    static void decode(Decoder& d, c::cpx& v)
+    {
+        if(avro::ResolvingDecoder* rd =
+               dynamic_cast<avro::ResolvingDecoder*>(&d))
+        {
+            const std::vector<size_t> fo = rd->fieldOrder();
+            for(std::vector<size_t>::const_iterator it = fo.begin();
+                it != fo.end();
+                ++it)
+            {
+                switch(*it)
+                {
+                case 0: avro::decode(d, v.re); break;
+                case 1: avro::decode(d, v.im); break;
+                default: break;
+                }
+            }
+        } else
+        {
+            avro::decode(d, v.re);
+            avro::decode(d, v.im);
+        }
+    }
+};
+} // namespace avro
+
+#include <stdlib.h>
 
 int main()
 {
-    boost::iostreams::filtering_streambuf<boost::iostreams::input> inbuf;
-    inbuf.push(boost::iostreams::gzip_decompressor());
+    std::unique_ptr<avro::OutputStream> out =
+        avro::memoryOutputStream();
+    avro::EncoderPtr e = avro::binaryEncoder();
+    e->init(*out);
+    c::cpx c1;
+    c1.re = "hello";
+    c1.im = "thanks";
+    avro::encode(*e, c1);
+
+    std::unique_ptr<avro::InputStream> in =
+        avro::memoryInputStream(*out);
+    avro::DecoderPtr d = avro::binaryDecoder();
+    d->init(*in);
+
+    c::cpx c2;
+    avro::decode(*d, c2);
+    std::cout << '(' << c2.re << ", " << c2.im << ')' << std::endl;
+    std::cout << "DECODED" << std::endl;
+    return 0;
 }
